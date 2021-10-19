d9021c00fc39ee8aa2588030ef936dac90a1593f
Matheo Deknuydt <matheodeknuydt@MacBook-Pro-de-Matheo.local>
2021-10-19 17:12:25 +0200

    utils: creation d'une classe utilitaire, pouvant être modifié par la suite si besoin

---
+++ RPGEnigma/Utils/ConsoleUtils.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RPGEnigma.Utils
+{
+    public class ConsoleUtils
+    {
+        public ConsoleUtils()
+        {
+        }
+
+        static void WriteMenuConsole(List<string> show)
+        {
+            for (int i = 0; i < show.Count; i++)
+            {
+                Console.WriteLine("{0} : {1}",i , show[i]);
+            }
+        }
+
+        static int AskPlayerReturnInt(string question)
+        {
+            int number;
+            do
+            {
+                Console.WriteLine(question);
+            } while (!int.TryParse(Console.ReadLine(), out number));
+            return number;
+        }
+
+        static char AskPlayerReturnChar(string question)
+        {
+            char letter;
+            do
+            {
+                Console.WriteLine(question);
+            } while (!char.TryParse(Console.ReadLine(), out letter));
+            return letter;
+        }
+    }
+}
