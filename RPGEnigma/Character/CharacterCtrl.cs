59154b3c97592bdfb27c7aab1d92d23dfc54aedc
Matheo Deknuydt <matheodeknuydt@macbook-pro-de-matheo.home>
2021-10-31 12:49:03 +0100

    feat: Classe d'un personnage avec la liaison de la classe Stats / utils: docker compose mariadb with phpmyadmin

---
+++ RPGEnigma/Character/CharacterCtrl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace RPGEnigma.Character
+{
+    public class CharacterCtrl
+    {
+        protected CharacterStat _stats;
+
+        protected int _level;
+
+        protected int _experienceLvl;
+
+        protected int _money;
+
+        public CharacterCtrl(CharacterStat stats)
+        {
+            _stats = stats;
+            _level = 0;
+            _experienceLvl = 0;
+            _money = 0;
+        }
+    }
+}
